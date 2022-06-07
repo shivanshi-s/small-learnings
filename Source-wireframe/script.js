06101a7cb154f6d4489aa276e7d1f3c82ff376a5
shivanshi <shivanshi.saxena2@gmail.com>
2022-06-07 16:14:23 +0530

    changes

---
+++ Source-wireframe/script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggle.addEventListener('click', () => {
+   navbarLinks.classList.toggle('active');
+})
